ea29c4511ae0f45c0e33f33e42309a1d7c5b2ab1
david-vargas98 <edgar981125@live.com.mx>
2025-09-06 11:35:42 -0600

    feat: import and export content pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client';
 import { Button } from '@carbon/react';
+import LandingPage from './home/page';
 
-export default function Home() {
-  return <Button>Button</Button>;
+export default function Page() {
+  return <LandingPage />;
 }
